622e797fe7c39bf960d6958ec6d1a040e1f3cde0
yuueuni <yuueuni@gmail.com>
2023-11-20 19:36:50 +0900

    init commit

---
+++ src/main/java/com/page/yoon/YoonPageApiApplication.java
@@ -0,0 +1,13 @@
+package com.page.yoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonPageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonPageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/page/yoon/YoonPageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.page.yoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonPageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
